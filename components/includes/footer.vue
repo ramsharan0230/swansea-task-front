ffa513f02e583dfe283f18fe19c3dd28bd1176eb
ram <ramsharan0230@gmail.com>
2025-07-05 10:24:25 +0100

    Contact and About us page created

--- components/includes/footer.vue
+++ components/includes/footer.vue
@@ -4,7 +4,7 @@
   >
     <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400"
       >© 2023
-      <a href="https://flowbite.com/" class="hover:underline">Profit Optimiser</a>. All
+      <a href="https://flowbite.com/" class="hover:underline">Smart Quotation</a>. All
       Rights Reserved.
     </span>
     <ul
--- components/includes/navbar.vue
+++ components/includes/navbar.vue
@@ -15,7 +15,7 @@
         />
         <span
           class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
-          >Profit Optimiser</span
+          >Smart Quotation</span
         ></nuxt-link
       >
 
