a134352b8c7b2947b13de54e1ab23b3b7a89bdd5
ram <ramsharan0230@gmail.com>
2025-07-05 09:59:11 +0100

    features: tailwind and flowbite integrated for better ui

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+// tailwind.config.js
+export default {
+  content: [
+    './components/**/*.{vue,js,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}',
+    './node_modules/flowbite/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
